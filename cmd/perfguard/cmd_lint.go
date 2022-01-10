6cd8dfa7c05c4bea5d70b46b0cd3c48a4676dd62
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2022-01-10 15:08:18 +0300

    cmd/perfguard: try to make findPackages more reliable (#54)

--- cmd/perfguard/cmd_lint.go
+++ cmd/perfguard/cmd_lint.go
@@ -9,12 +9,7 @@ func cmdLint(stdout, stderr io.Writer, args []string) error {
 	r := newRunner(stdout, stderr)
 
 	fs := flag.NewFlagSet("perfguard optimize", flag.ExitOnError)
-	fs.BoolVar(&r.autofix, "fix", false,
-		`apply the suggested fixes automatically, where possible`)
-	fs.StringVar(&r.goVersion, "go", "",
-		`select the Go version to target; leave as string for the latest`)
-	fs.BoolVar(&r.absFilenames, "abs", false,
-		`print absolute filenames in the output`)
+	addCommonFlags(r, fs)
 	noColor := fs.Bool("no-color", false, `disable colored output`)
 	_ = fs.Parse(args)
 
