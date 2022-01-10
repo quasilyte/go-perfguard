15e9f84e30a73a541fd61c410152b803a6bf2de9
Iskander Sharipov <quasilyte@gmail.com>
2022-01-10 11:51:23 +0300

    add a ci check that rulesdata is up to date

---
+++ _script/check.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	var c checker
+
+	steps := []struct {
+		name string
+		fn   func() error
+	}{
+		{"check autogen", c.checkAutogen},
+	}
+	for _, step := range steps {
+		if err := step.fn(); err != nil {
+			log.Fatalf("%s: error: %v", step.name, err)
+		}
+	}
+}
+
+type checker struct{}
+
+func (c *checker) checkAutogen() error {
+	filenames := []string{
+		"opt_rules.go",
+		"universal_rules.go",
+	}
+	readContents := func() ([][]byte, error) {
+		var result [][]byte
+		for _, filename := range filenames {
+			data, err := os.ReadFile(filepath.Join("perfguard", "rulesdata", filename))
+			if err != nil {
+				return nil, err
+			}
+			result = append(result, data)
+		}
+		return result, nil
+	}
+
+	contents, err := readContents()
+	if err != nil {
+		return err
+	}
+
+	out, err := exec.Command("go", "generate", "./perfguard").CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("run go generate: %v: %s", err, out)
+	}
+
+	newContents, err := readContents()
+	if err != nil {
+		return err
+	}
+
+	for i, filename := range filenames {
+		data := contents[i]
+		newData := newContents[i]
+		if !bytes.Equal(data, newData) {
+			return fmt.Errorf("rulesdata for %s is outdated, run 'go generate ./perfguard'", filename)
+		}
+	}
+
+	return nil
+}
