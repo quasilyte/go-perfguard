befd32ecc0e51ab3345a2d8d8f5d5ad4783047db
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2022-01-25 17:16:38 +0300

    perfguard/checkers: add bigArgCopy checker (#135)

--- internal/goutil/goutil.go
+++ internal/goutil/goutil.go
@@ -2,7 +2,36 @@ package goutil
 
 import (
 	"go/ast"
+	"go/types"
 )
+
+func TypeHasPointers(typ types.Type) bool {
+	switch typ := typ.(type) {
+	case *types.Basic:
+		switch typ.Kind() {
+		case types.UnsafePointer, types.String, types.UntypedNil, types.UntypedString:
+			return true
+		}
+		return false
+
+	case *types.Named:
+		return TypeHasPointers(typ.Underlying())
+
+	case *types.Struct:
+		for i := 0; i < typ.NumFields(); i++ {
+			if TypeHasPointers(typ.Field(i).Type()) {
+				return true
+			}
+		}
+		return false
+
+	case *types.Array:
+		return TypeHasPointers(typ.Elem())
+
+	default:
+		return true
+	}
+}
 
 func ContainsIdent(root ast.Node, x *ast.Ident) bool {
 	return Contains(root, func(n ast.Node) bool {
---
+++ perfguard/checkers/callcheckers/bigArgCopy.go
@@ -0,0 +1,62 @@
+package callcheckers
+
+import (
+	"fmt"
+	"go/ast"
+	"go/types"
+
+	"github.com/quasilyte/go-perfguard/internal/goutil"
+	"github.com/quasilyte/go-perfguard/perfguard/checkers"
+	"github.com/quasilyte/go-perfguard/perfguard/lint"
+)
+
+func init() {
+	doc := checkers.Doc{
+		Name:  "bigArgCopy",
+		Score: 2,
+	}
+	checkers.RegisterCallChecker(doc, func() checkers.CallChecker {
+		return &bigArgCopyChecker{}
+	})
+}
+
+type bigArgCopyChecker struct{}
+
+func (c *bigArgCopyChecker) CheckCall(ctx *lint.Context, call *ast.CallExpr) error {
+	if len(call.Args) == 0 {
+		return nil
+	}
+
+	fnType, ok := ctx.TypeOf(call.Fun).(*types.Signature)
+	if !ok {
+		return nil
+	}
+	numParams := fnType.Params().Len()
+	for i, arg := range call.Args {
+		if i >= numParams {
+			break
+		}
+		param := fnType.Params().At(i)
+		if param.Name() == "" || param.Name() == "_" {
+			continue
+		}
+		if !c.isBig(ctx, param.Type()) {
+			continue
+		}
+		ctx.Report(lint.ReportParams{
+			PosNode: arg,
+			Message: fmt.Sprintf("expensive %s arg copy (%d bytes), consider passing it by pointer",
+				param.Name(), ctx.Target.Sizes.Sizeof(param.Type())),
+		})
+	}
+
+	return nil
+}
+
+func (c *bigArgCopyChecker) isBig(ctx *lint.Context, typ types.Type) bool {
+	size := ctx.Target.Sizes.Sizeof(typ)
+	if goutil.TypeHasPointers(typ) {
+		return size > 160
+	}
+	return size > 320
+}
