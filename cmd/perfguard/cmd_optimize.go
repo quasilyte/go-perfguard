e1d67355f151905bdcbfc3638e96d01c12176a08
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2022-01-25 15:44:23 +0300

    cmd/perfguard: increase the default --heatmap-threshold (#134)

--- cmd/perfguard/cmd_optimize.go
+++ cmd/perfguard/cmd_optimize.go
@@ -13,7 +13,7 @@ func cmdOptimize(stdout, stderr io.Writer, args []string) error {
 	addCommonFlags(r, fs)
 	fs.StringVar(&r.args.heatmapFile, "heatmap", "",
 		`a CPU profile that will be used to build a heatmap, needed for IsHot() filters`)
-	fs.Float64Var(&r.args.heatmapThreshold, "heatmap-threshold", 0.25,
+	fs.Float64Var(&r.args.heatmapThreshold, "heatmap-threshold", 0.5,
 		`a threshold argument used to create a heatmap, see perf-heatmap docs on it`)
 	noColor := fs.Bool("no-color", false, `disable colored output`)
 	_ = fs.Parse(args)
